38335fa7cf53e004b7a52743957140ac8a0eead5
omsurti <125464391+techhelpsOm@users.noreply.github.com>
2023-02-22 10:37:21 +0530

    'initial_commit'

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const sh = require("shelljs")
 require("dotenv").config()
 
 // gh repo create my-project --private --source=. --remote=upstream
-sh.exec("git init")
+// sh.exec("git init")
+// sh.exec("git add .")
+// sh.exec("git commit -m 'initial_commit'")
+
+// sh.exec(`gh repo create ${process.env.NAME} --private`)
 sh.exec("git add .")
 sh.exec("git commit -m 'initial_commit'")
-
-sh.exec(`gh repo create ${process.env.NAME} --private --source=. --remote=upstream`)
+sh.exec("git push -u origin master")
 
 // sh.exec()
